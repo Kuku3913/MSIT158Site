659019688e36d64e0a5d6d7a7f5afc77497495e8
Doris <doris19950213@gmail.com>
2024-05-21 17:05:03 +0800

    加入專案檔案。

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MSIT158Site.Models;
+
+namespace MSIT158Site.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly MyDBContext _context;
+
+        public ProductsController(MyDBContext context) 
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
